8437f30dc7142dc1b91b6c9be67bc633a527087a
Marc J. Schmidt <marc@marcjschmidt.de>
2017-02-23 22:52:57 +0100

    add some delay

--- model.py
+++ model.py
@@ -1,9 +1,13 @@
 import aetros.backend
 import math
+import time
 
 job = aetros.backend.start_job('marcj/test-tpe')
 
 kpi = job.create_channel('kpi', kpi=True, main=True, max_optimization=False)
-kpi.send(1, math.sin(job.get_parameter('x')))
+kpi.send(1, 1)
+time.sleep(2)
+kpi.send(2, math.sin(job.get_parameter('x')))
+time.sleep(2)
 
 job.done()
