585d246d9ff920bad1724adf8823e7a272d14092
Marc J. Schmidt <marc@marcjschmidt.de>
2017-02-25 18:02:48 +0100

    add printing stuff

--- model.py
+++ model.py
@@ -6,8 +6,11 @@ job = aetros.backend.start_job('marcj/test-tpe')
 
 kpi = job.create_channel('kpi', kpi=True, main=True, max_optimization=False)
 kpi.send(1, 1)
+print('kpi 1')
 time.sleep(2)
 kpi.send(2, math.sin(job.get_parameter('x')))
+print('kpi ' + str(math.sin(job.get_parameter('x'))))
 time.sleep(2)
 
+print('done')
 job.done()
