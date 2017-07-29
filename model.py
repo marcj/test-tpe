42f292b94abab956fcbcee0454aef5c7d73cb93f
Marc J. Schmidt <marc@marcjschmidt.de>
2017-07-29 18:11:38 +0200

    added progress

--- model.py
+++ model.py
@@ -6,11 +6,16 @@ job = aetros.backend.start_job('marcj/test-tpe')
 
 kpi = job.create_channel('kpi', kpi=True, main=True, max_optimization=False)
 kpi.send(1, 1)
+job.progress(1,3)
 print('kpi 1')
+
 time.sleep(2)
 kpi.send(2, math.sin(job.get_parameter('x')))
+job.progress(2, 3)
+
 print('kpi ' + str(math.sin(job.get_parameter('x'))))
 time.sleep(2)
+job.progress(3, 3)
 
 print('done')
 job.done()
