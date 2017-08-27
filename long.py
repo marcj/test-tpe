c5547be27bfbc1bd9494391a018eab0dfad5b194
Marc J. Schmidt <marc@marcjschmidt.de>
2017-08-27 22:03:54 +0200

    add long.py

---
+++ long.py
@@ -0,0 +1,14 @@
+import aetros.backend
+import math
+import time
+
+job = aetros.backend.start_job('marcj/asd-asdasd/long-running')
+kpi = job.create_channel('kpi', kpi=True, main=True, max_optimization=False)
+
+for i in range(1, 2000):
+	kpi.send(i, i*233/4*time.time())
+	job.progress(i, 1000)
+	time.sleep(2)
+
+print('done')
+job.done()
